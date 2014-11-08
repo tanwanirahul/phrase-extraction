b80bb136fbe0cabcc55f4809a86cbf0af0749e26
tanwairahul <tanwanirahul@gmail.com>
2014-11-09 02:28:40 +0530

    Adding argument parsing and updated the summary

--- src/main.py
+++ src/main.py
@@ -3,13 +3,27 @@ Created on 08-Nov-2014
 
 @author: Rahul
 
-@summary: The main file for the project.
-          Firstly, calculates the PMI scores for Bi, Tri and Quad grams.
-          Then waits for the user's input.
-          Lastly, for the given text, computes Ngrams and also the
-          classification as valid or invalid phrase.
+@summary: The project supports two options:
+        1. Repl - Enter into repl to quickly find valid NGram phrases by providing text.
+        2. Prepare - Useful to compute NGrams and their collocation score from the development data.
 '''
 from core.chunker import score_and_save_ngrams
+from core.repl import repl
+
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
-    score_and_save_ngrams()
+    parser = ArgumentParser(description='Find the valid NGram phrases.')
+
+    parser.add_argument('-r', '--repl', help="Enter into the REPL mode",
+                        dest='repl', action='store_true')
+
+    parser.add_argument('-p', '--prepare', help="Prepare NGrams and Scores",
+                        dest='prepare', action='store_true')
+
+    args = parser.parse_args()
+
+    if args.repl:
+        repl()
+    elif args.prepare:
+        score_and_save_ngrams()
